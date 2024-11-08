448f36cde82a34f333ccfee36735e1a3761e54f4
umittadelen <umittadelen1277@gmail.com>
2024-11-08 09:05:49 +0100

    demo of github pages

---
+++ demo/app.py
@@ -0,0 +1,288 @@
+# import the required libraries
+import utils
+utils.check_and_install()
+
+from flask import Flask, render_template, request, send_file, jsonify
+import torch
+import random
+from diffusers import (
+    StableDiffusionXLPipeline,
+    AutoencoderKL
+)
+from PIL import PngImagePlugin, Image
+import os
+import math
+import logging
+import time
+import threading
+from config import Config
+from nudenet import NudeDetector
+import sys
+import subprocess
+import glob
+from io import BytesIO
+import gc
+
+config = Config()
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask(__name__)
+
+def load_pipeline(model_name):
+    config.imgprogress = "Loading Pipeline..."
+
+    if model_name not in config.model_cache:
+        config.imgprogress = "Loading New Pipeline..."
+        config.model_cache = {}
+
+        config.imgprogress = "Loading New Pipeline... (loading VAE)"
+        # Load the VAE model
+        vae = AutoencoderKL.from_pretrained(
+            "madebyollin/sdxl-vae-fp16-fix",
+            torch_dtype=torch.float16,
+        )
+        config.imgprogress = "Loading New Pipeline... (VAE loaded)"
+
+        config.imgprogress = "Loading New Pipeline... (loading Pipeline)"
+        # Set the pipeline
+        pipeline = (
+            StableDiffusionXLPipeline.from_single_file
+            if model_name.endswith(".safetensors")
+            else StableDiffusionXLPipeline.from_pretrained
+        )
+        config.imgprogress = "Loading New Pipeline... (Pipeline loaded)"
+
+        config.imgprogress = "Loading New Pipeline... (pipe)"
+        # Load the pipeline
+        pipe = pipeline(
+            model_name,
+            vae=vae,
+            torch_dtype=torch.float16,
+            custom_pipeline="lpw_stable_diffusion_xl",
+            use_safetensors=True,
+            add_watermarker=False,
+            use_auth_token=config.HF_TOKEN
+        )
+        config.imgprogress = "Loading New Pipeline... (pipe loaded)"
+
+        pipe.to('cuda' if torch.cuda.is_available() else 'cpu')
+
+        config.model_cache[model_name] = pipe
+        config.imgprogress = "Pipeline Loaded..."
+        return pipe
+    else:
+        config.imgprogress = "Using Cached Pipeline..."
+        return config.model_cache[model_name]
+
+def generateImage(pipe, prompt, original_prompt, negative_prompt, seed, width, height, cfg_scale, samplingSteps):
+    # Generate image with progress tracking
+
+    detector = NudeDetector()
+
+    def progress(pipe, step_index, timestep, callback_kwargs):
+        config.imgprogress = int(math.floor(step_index / samplingSteps * 100))
+        config.allPercentage = int(math.floor((config.IMAGE_COUNT - config.remainingImages + (step_index / samplingSteps)) / config.IMAGE_COUNT * 100))
+
+        if config.generation_stopped:
+            config.imgprogress = "Generation Stopped"
+            config.allPercentage = 0
+            raise StopIteration
+        
+        return callback_kwargs
+
+    config.imgprogress = "Generating Image..."
+
+    try:
+        image = pipe(
+            prompt,
+            negative_prompt=negative_prompt,
+            width=width,
+            height=height,
+            guidance_scale=cfg_scale,
+            num_inference_steps=samplingSteps,
+            generator=torch.manual_seed(seed),
+            callback_on_step_end=progress
+        ).images[0]
+
+        metadata = PngImagePlugin.PngInfo()
+        metadata.add_text("Prompt", prompt)
+        metadata.add_text("OriginalPrompt", original_prompt)
+        metadata.add_text("NegativePrompt", negative_prompt)
+        metadata.add_text("Width", str(width))
+        metadata.add_text("Height", str(height))
+        metadata.add_text("CFGScale", str(cfg_scale))
+        metadata.add_text("Seed", str(seed))
+        metadata.add_text("SamplingSteps", str(samplingSteps))
+        metadata.add_text("Model", str(list(config.model_cache.keys())[0]))
+
+        # Save the image to the temporary directory
+        image_path = os.path.join(config.generated_dir, f'image{time.time()}_{seed}.png')
+        image.save(image_path, 'PNG', pnginfo=metadata)
+
+        detection_results = detector.detect(image_path)
+        config.imgprogress = "DONE"
+
+        # Define sensitive classes
+        sensitive_classes = {
+            "FEMALE_GENITALIA_COVERED",
+            "BUTTOCKS_EXPOSED",
+            "FEMALE_BREAST_EXPOSED",
+            "FEMALE_GENITALIA_EXPOSED",
+            "MALE_BREAST_EXPOSED",
+            "ANUS_EXPOSED",
+            "BELLY_COVERED",
+            "BELLY_EXPOSED",
+            "MALE_GENITALIA_EXPOSED",
+            "ANUS_COVERED",
+            #"FEMALE_BREAST_COVERED",
+            "BUTTOCKS_COVERED"
+        }
+
+        return image_path, next((detection['class'] for detection in detection_results if detection['class'] in sensitive_classes), False)
+    
+    except StopIteration:
+        # If generation was stopped, handle it gracefully
+        config.imgprogress = "Generation Manually Stopped"
+        return False, False
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    # Check if generation is already in progress
+    if config.generating:
+        return jsonify(status='Image generation already in progress'), 400
+
+    config.generating = True
+    config.generated_image = {}
+    config.imgprogress = "Starting Image Generation..."
+
+    # Get parameters from the request
+    model_name = request.form['model']
+    original_prompt = request.form['prompt']
+    prompt = utils.preprocess_prompt(request.form['prompt']) if request.form.get("prompt_helper", "OFF") == "ON" else request.form['prompt']
+    negative_prompt = str(request.form['negative_prompt'])
+    width = int(request.form.get('width', 832))
+    height = int(request.form.get('height', 1216))
+    cfg_scale = float(request.form.get('cfg_scale', 7))
+    config.IMAGE_COUNT = int(request.form.get('image_count', 4))
+    config.CUSTOM_SEED = int(request.form.get('custom_seed', 0))
+    samplingSteps = int(request.form.get('sampling_steps', 28))
+
+    if config.CUSTOM_SEED != 0:
+        config.IMAGE_COUNT = 1
+
+    # Function to generate images
+    def generate_images():
+        # Load the model pipeline
+        try:
+            pipe = load_pipeline(model_name)
+        except Exception as e:
+            config.generating = False
+            config.imgprogress = "Error Loading Model..."
+            config.allPercentage = 0
+            return jsonify(status=f"Error loading model: {str(e)}"), 500
+        
+        try:
+            for i in range(config.IMAGE_COUNT):
+                if config.generation_stopped:
+                    config.allPercentage = 0
+                    config.imgprogress = "Generation Stopped"
+                    config.generating = False
+                    config.generation_stopped = False
+                    break
+
+                # Update the progress message
+                config.remainingImages = config.IMAGE_COUNT - i
+                config.imgprogress = f"Generating {config.remainingImages} Images..."
+                config.allPercentage = 0
+
+                # Generate a new seed for each image
+                if config.CUSTOM_SEED == 0:
+                    seed = random.randint(0, 100000000000)
+                else:
+                    seed = config.CUSTOM_SEED
+
+                image_path, sensitive = generateImage(pipe, prompt, original_prompt, negative_prompt, seed, width, height, cfg_scale, samplingSteps)
+
+                # Store the generated image path
+                if image_path:
+                    config.generated_image[seed] = [image_path, sensitive]
+        finally:
+            del pipe
+            config.model_cache = {}
+            torch.cuda.ipc_collect()
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        config.imgprogress = "Generation Complete"
+        config.allPercentage = 100
+        config.generating = False
+
+    # Start image generation in a separate thread to avoid blocking
+    threading.Thread(target=generate_images).start()
+
+    return jsonify(status='Image generation started', count=config.IMAGE_COUNT)
+
+@app.route('/status', methods=['GET'])
+def status():
+    # Convert the generated images to a list to send to the client
+    images =[
+        {
+            'img': path[0],
+            'seed': seed,
+            'sensitive': path[1]
+        } for seed, path in config.generated_image.items()]
+    
+    return jsonify(images=images, imgprogress=config.imgprogress, allpercentage=config.allPercentage)
+
+@app.route('/generated/<filename>', methods=['GET'])
+def serve_temp_image(filename):
+    size = request.args.get('size')
+    image_path = os.path.join(config.generated_dir, filename)
+
+    if size == 'small':
+        with Image.open(image_path) as img:
+            new_size = (img.width // 3, img.height // 3)
+            img = img.resize(new_size, Image.LANCZOS)
+
+            img_io = BytesIO()
+            img.save(img_io, format='PNG')
+            img_io.seek(0)
+
+            return send_file(img_io, mimetype='image/png')
+    
+    return send_file(image_path, mimetype='image/png')
+
+@app.route('/image/<filename>', methods=['GET'])
+def image(filename):
+    return render_template('image.html', image=filename)
+
+@app.route('/stop', methods=['POST'])
+def stop_generation():
+    config.generation_stopped = True
+    return jsonify(status='Image generation stopped')
+
+@app.route('/clear', methods=['POST'])
+def clear_images():
+    config.generated_image = {}
+    files = glob.glob(os.path.join(config.generated_dir, '*'))
+    for file in files:
+        try:
+            os.remove(file)
+        except Exception as e:
+            config.allPercentage = 0
+            config.imgprogress = f"Error Deleteing File... {e}"
+    return jsonify(status='Images cleared')
+
+@app.route('/restart', methods=['POST'])
+def restart_app():
+    config.allPercentage = 0
+    # Spawn a new process of the current script
+    subprocess.Popen([sys.executable] + sys.argv)
+    # Exit the current process
+    os._exit(0)
+
+if __name__ == '__main__':
+    app.run(host='192.168.0.4', port=8080, debug=False)
+    config.imgprogress = "Server Started"
---
+++ demo/page/js/main.js
@@ -0,0 +1,237 @@
+const existingImages = new Map(); // Store existing images with their seeds as keys
+const sensitiveToggle = document.getElementById('sensitive-toggle');
+
+let isGeneratingNewImages = false;
+let serverUrl = getServerUrl();
+
+document.getElementById('generateForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Set the flag to true when a new generation starts
+    isGeneratingNewImages = true;
+
+    // Clear existing images for new generation
+    existingImages.clear();
+    document.getElementById('images').innerHTML = '';
+
+    const formData = new FormData(this);
+
+    fetch(`${serverUrl}/generate`, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.seed) {
+            globalSeed = data.seed; // Store the seed globally if necessary
+        }
+
+        // Set the flag to false when the generation request is acknowledged
+        isGeneratingNewImages = false;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        isGeneratingNewImages = false; // Reset the flag in case of an error
+    });
+});
+
+setInterval(() => {
+    // Check if the document is visible before fetching the status
+    if (document.visibilityState === 'visible') {
+        fetch(`${serverUrl}/status`, { cache: 'no-store' })
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('all').style.display = 'flex';
+
+                const imagesDiv = document.getElementById('images');
+                const progressText = document.getElementById('progress');
+                const dynamicProgressBar = document.getElementById('dynamic-progress-bar');
+                const alldynamicProgressBar = document.getElementById('all-dynamic-progress-bar');
+
+                // Update progress value smoothly
+                if (Number.isInteger(data.imgprogress)) {
+                    dynamicProgressBar.style.width = `${data.imgprogress}%`;
+                    progressText.innerHTML = `Progress: ${data.imgprogress}%`;
+                } else {
+                    dynamicProgressBar.style.width = `0%`;
+                    alldynamicProgressBar.style.width = `0%`;
+                    progressText.innerHTML = `Progress: ${data.imgprogress}`;
+                }
+
+                if (Number.isInteger(data.allpercentage)) {
+                    alldynamicProgressBar.style.width = `${data.allpercentage}%`;
+                } else {
+                    alldynamicProgressBar.style.width = `0%`;
+                }
+
+                // Only process images if not currently generating new ones
+                if (!isGeneratingNewImages && data.images.length > 0) {
+                    data.images.forEach(imgData => {
+                        const key = imgData.seed; // Assuming seed is the unique identifier for the image
+
+                        if (existingImages.has(key)) {
+                            // Update existing image
+                            const existingWrapper = existingImages.get(key);
+                            const existingImg = existingWrapper.querySelector('img');
+                            existingImg.src = imgData.img + "?size=small";
+
+                            // Update the blur effect based on the toggle
+                            updateBlurEffect(existingImg, imgData.sensitive);
+                        } else {
+                            // Create new image element if it doesn't exist
+                            const wrapper = document.createElement('div');
+                            wrapper.className = 'image-wrapper';
+
+                            const img = document.createElement('img');
+                            img.src = imgData.img + "?size=small";
+                            img.loading = "lazy";
+
+                            // Create sensitive text if necessary
+                            if (imgData.sensitive) {
+                                const sensitiveText = document.createElement('p');
+                                sensitiveText.classList.add('centered');
+                                sensitiveText.innerHTML = imgData.sensitive;
+                                wrapper.appendChild(sensitiveText);
+                            }
+
+                            // Apply initial blur effect
+                            updateBlurEffect(img, imgData.sensitive);
+
+                            img.onclick = () => {
+                                openLink("image/" + imgData.img.split('/').pop()); // Send to "image/" + filename
+                            };
+
+                            wrapper.appendChild(img); // Add image to wrapper
+                            imagesDiv.appendChild(wrapper); // Add wrapper to imagesDiv
+
+                            existingImages.set(key, wrapper); // Store the wrapper in the map
+                        }
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching status:', error);
+                document.getElementById('all').style.display = 'none';
+            });
+    }
+}, 1500); // Check every 1.5 seconds
+
+document.getElementById('stopButton').addEventListener('click', function() {
+    fetch(`${serverUrl}/stop`, {
+        method: 'POST'
+    })
+    .catch(error => console.error('Error stopping generation:', error));
+});
+
+document.getElementById('restartButton').addEventListener('click', function() {
+    fetch(`${serverUrl}/restart`, {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('all').style.display = 'none';
+    })
+    .catch(error => console.error('Error stopping generation:', error));
+});
+
+document.getElementById('clearButton').addEventListener('click', function() {
+    const isConfirmed = confirm('Are you sure you want to clear all images?');
+
+    if (isConfirmed) {
+        fetch('/clear', {
+            method: 'POST'
+        })
+        .then(response => response.json())
+        .then(data => {
+            existingImages.clear();
+            document.getElementById('images').innerHTML = '';
+        })
+        .catch(error => console.error('Error Clearing Images:', error));
+    }
+});
+
+document.getElementById('ChangeIpButton').addEventListener('click', () => {
+    const newIpPort = prompt("Enter the server IP and port (format: ip:port):");
+    if (newIpPort) {
+        localStorage.setItem('serverIpPort', newIpPort);
+    }
+    serverUrl = getServerUrl();
+});
+
+// Function to get the stored IP and port or a default value
+function getServerUrl() {
+    return `http://${localStorage.getItem('serverIpPort') || '127.0.0.1:5000'}`;
+}
+
+function updateBlurEffect(img, isSensitive) {
+    if (isSensitive && !sensitiveToggle.checked) {
+        img.classList.add('blurred'); // Add blur class if sensitive and toggle is off
+    } else {
+        img.classList.remove('blurred'); // Remove blur class if toggle is on or not sensitive
+    }
+}
+
+function openLink(link) {
+    window.location.href = link.replace("?size=small", "");
+}
+
+sensitiveToggle.addEventListener('change', () => {
+    if (sensitiveToggle.checked) {
+        // Ask for confirmation if the user is 18+
+        const isAdult = confirm('Are you 18 years or older?');
+
+        if (!isAdult) {
+            // If user clicks "No", uncheck the toggle switch
+            sensitiveToggle.checked = false;
+            return; // Exit the function, no further action needed
+        }
+    }
+
+    // Apply the blur effect based on the user's choice
+    existingImages.forEach((wrapper) => {
+        const img = wrapper.querySelector('img');
+        const isSensitive = wrapper.querySelector('.centered') !== null; // Check if sensitive text exists
+        updateBlurEffect(img, isSensitive);
+    });
+});
+
+//TODO handle prompt example change
+
+const promptSelectElement = document.getElementById('example_prompt');
+const promptTextareaElement = document.getElementById('prompt');
+
+// Add an event listener for the 'change' event on the select element
+promptSelectElement.addEventListener('change', function() {
+    promptTextareaElement.value = promptSelectElement.value; // Update textarea with the selected prompt
+});
+
+// Set the initial value of the textarea to the first option's value
+promptTextareaElement.value = promptSelectElement.value;
+
+//TODO handle pre dimension change
+
+// Assuming the following elements exist in your HTML
+const exampleSizeSelectElement = document.getElementById('example_size');
+const widthInputElement = document.getElementById('width');
+const heightInputElement = document.getElementById('height');
+
+// Add an event listener for the 'change' event on the example_size select element
+exampleSizeSelectElement.addEventListener('change', function() {
+    const selectedOption = exampleSizeSelectElement.options[exampleSizeSelectElement.selectedIndex];
+
+    if (selectedOption) {
+        // Parse the selected value to get width and height
+        const dimensions = selectedOption.value.split('x'); // Assuming the value format is "widthxheight"
+        if (dimensions.length === 2) {
+            widthInputElement.value = dimensions[0]; // Set width
+            heightInputElement.value = dimensions[1]; // Set height
+        }
+    }
+});
+
+// Optionally, set the initial values based on the first option in the select
+if (exampleSizeSelectElement.options.length > 0) {
+    const initialDimensions = exampleSizeSelectElement.options[0].value.split('x');
+    widthInputElement.value = initialDimensions[0];
+    heightInputElement.value = initialDimensions[1];
+}
